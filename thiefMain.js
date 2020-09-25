42153b653dd345aa0e61762adc30c615e9b456fb
Vladislav Lapko <vlapko@vba.com>
2020-09-25 15:13:51 +0200

    Version 1.1

--- thiefMain.js
+++ thiefMain.js
@@ -1,6 +1,13 @@
-const textElement = document.getElementById('text')
-const optionButtonsElement = document.getElementById('option-buttons')
-
+// Edit your script here
+window.onload = onPageLoaded;
+var textElement =null;
+var optionButtonsElement =null;
+function onPageLoaded()
+{
+ 	textElement = document.getElementById("text");
+ 	optionButtonsElement = document.getElementById('option-buttons')
+startGame();
+}
 let state = {}
 
 function startGame() {
@@ -14,8 +21,9 @@ function showTextNode(textNodeIndex) {
   if (textNode.setState != null) {
     state = Object.assign(state, textNode.setState)
   }
-
+console.log(textElement);
   textElement.innerText = textNode.text
+
   while (optionButtonsElement.firstChild) {
     optionButtonsElement.removeChild(optionButtonsElement.firstChild)
   }
@@ -48,156 +56,156 @@ function changeState(textNode) {}
 
 
 const textNodes = [{
-      id: 1,
-      text: 'Вы вор Владислав. Вы забрались в квартиру своего дяди в шабанах и стоите в прихожей.',
-      setState: {
-        prihod: true
+    id: 1,
+    text: 'Вы вор Владислав. Вы забрались в квартиру своего дяди в шабанах и стоите в прихожей.',
+    setState: {
+      prihod: true
+    },
+    options: [{
+        text: 'Осмотреться',
+        nextText: 3
       },
-      options: [{
-          text: 'Осмотреться',
-          nextText: 3
+      {
+        text: 'Зал',
+        nextText: 12
+      },
+      {
+        text: 'Туалет',
+        nextText: 4
+      }
+    ]
+  },
+  {
+    id: 2,
+    text: 'Вы все еще вор Владислав и стоите в прихожей своего дяди. В шабанах.',
+    options: [{
+        text: 'Осмотреться',
+        nextText: 3
+      },
+      {
+        text: 'Зал',
+        nextText: 12
+      },
+      {
+        text: 'Туалет',
+        requiredState: (currentState) => currentState.checkWC == false,
+        nextText: 4
+      },
+      {
+        text: 'Туалет',
+        requiredState: (currentState) => currentState.checkWC == true,
+        nextText: 9
+      }
+    ]
+  },
+  {
+    id: 3,
+    text: 'Перестань осматриваться, действуй.',
+    options: [{
+        text: 'Пойти в туалет',
+        requiredState: (currentState) => currentState.checkWC == false,
+        nextText: 4
+      },
+      {
+        text: 'Пойти в туалет',
+        requiredState: (currentState) => currentState.checkWC == true,
+        nextText: 9
+      },
+      {
+        text: 'Пойти в зал',
+        nextText: 12
+      }
+    ]
+  },
+  {
+    id: 4,
+    text: 'Только вы приблизились к двери, как услышали за ней подозрительный шорох. Возможно это ваш дядя и если он вас увидит, то вам несдобровать.',
+    options: [{
+        text: 'Рискнуть и зайти в туалет',
+        setState: {
+          goldenKey: true,
+          checkWC: true
         },
-        {
-          text: 'Зал',
-          nextText: 12
-        },
-        {
-          text: 'Туалет',
-          nextText: 4
-        }
-      ]
-    },
-    {
-      id: 2,
-      text: 'Вы все еще вор Владислав и стоите в прихожей своего дяди. В шабанах.',
-      options: [{
-          text: 'Осмотреться',
-          nextText: 3
-        },
-        {
-          text: 'Зал',
-          nextText: 12
-        },
-        {
-          text: 'Туалет',
-          requiredState: (currentState) => currentState.checkWC == false,
-          nextText: 4
-        },
-        {
-          text: 'Туалет',
-          requiredState: (currentState) => currentState.checkWC == true,
-          nextText: 9
-        }
-      ]
-    },
-    {
-      id: 3,
-      text: 'Перестань осматриваться, действуй.',
-      options: [{
-          text: 'Пойти в туалет',
-          requiredState: (currentState) => currentState.checkWC == false,
-          nextText: 4
-        },
-        {
-          text: 'Пойти в туалет',
-          requiredState: (currentState) => currentState.checkWC == true,
-          nextText: 9
-        },
-        {
-          text: 'Пойти в зал',
-          nextText: 12
-        }
-      ]
-    },
-    {
-      id: 4,
-      text: 'Только вы приблизились к двери, как услышали за ней подозрительный шорох. Возможно это ваш дядя и если он вас увидит, то вам несдобровать.',
-      options: [{
-          text: 'Рискнуть и зайти в туалет',
-          setState: {
-            goldenKey: true,
-            checkWC: true
-          },
-          nextText: 9
-        },
-        {
-          text: 'Вернуться в прихожую',
-          nextText: 2
-        }
-      ]
-    },
-    {
-      id: 9,
-      text: 'В туалете больше нет ничего ценного. Шорох из трубы кажется уже не таким подозрительным. ',
-      options: [{
+        nextText: 9
+      },
+      {
         text: 'Вернуться в прихожую',
         nextText: 2
-      }]
+      }
+    ]
+  },
+  {
+    id: 9,
+    text: 'В туалете больше нет ничего ценного. Шорох из трубы кажется уже не таким подозрительным. ',
+    options: [{
+      text: 'Вернуться в прихожую',
+      nextText: 2
+    }]
+  },
+  {
+    id: 5,
+    text: 'Дядя медленно зашел в туалет. Медленно закрыл дверь. Медленно достал дробовик. И быстро распределил ваши внутренности по кафельной плитке.',
+    options: [{
+      text: 'КОНЕЦ.',
+      nextText: -1
+    }]
+  },
+  {
+    id: 6,
+    text: 'Вы в тупике. Вы гений.',
+    options: [{
+      text: 'Вспомнить лучшие моменты жизни',
+      nextText: 5
+    }]
+  },
+  {
+    id: 7,
+    text: 'Дядя медленно зашел на кухню. Медленно закрыл дверь. Медленно достал дробовик. И быстро распределил ваши внутренности по дверце холодильника.',
+    options: [{
+      text: 'КОНЕЦ.',
+      nextText: -1
+    }]
+  },
+  {
+    id: 8,
+    text: 'Вы не взяли во внимание, что дядя живет на 6ом этаже. Вы разбились и умерли.',
+    options: [{
+      text: 'КОНЕЦ.',
+      nextText: -1
+    }]
+  },
+  {
+    id: 10,
+    text: 'Молодец. Вы застелили дядину постель.',
+    setState: {
+      makeBed: 1
     },
-    {
-      id: 5,
-      text: 'Дядя медленно зашел в туалет. Медленно закрыл дверь. Медленно достал дробовик. И быстро распределил ваши внутренности по кафельной плитке.',
-      options: [{
-        text: 'КОНЕЦ.',
-        nextText: -1
-      }]
-    },
-    {
-      id: 6,
-      text: 'Вы в тупике. Вы гений.',
-      options: [{
-        text: 'Вспомнить лучшие моменты жизни',
-        nextText: 5
-      }]
-    },
-    {
-      id: 7,
-      text: 'Дядя медленно зашел на кухню. Медленно закрыл дверь. Медленно достал дробовик. И быстро распределил ваши внутренности по дверце холодильника.',
-      options: [{
-        text: 'КОНЕЦ.',
-        nextText: -1
-      }]
-    },
-    {
-      id: 8,
-      text: 'Вы не взяли во внимание, что дядя живет на 6ом этаже. Вы разбились и умерли.',
-      options: [{
-        text: 'КОНЕЦ.',
-        nextText: -1
-      }]
-    },
-    {
-      id: 10,
-      text: 'Молодец. Вы застелили дядину постель.',
-      setState: {
-        makeBed: 1
+    options: [{
+        text: 'Отойти от постели',
+        nextText: 12 //zal tut nado
       },
-      options: [{
-          text: 'Отойти от постели',
-          nextText: 12 //zal tut nado
-        },
-        {
-          text: 'Расстелить обратно',
-          nextText: 15 //rasstilka posteli
-        }
-      ]
-    }, {
-      id: 11,
-      text: 'Молодец. Вы застелили дядину постель. А на полу нашли ржавый ключ.',
-      setState: {
-        rustyKey: true,
-        makeBed: 1
+      {
+        text: 'Расстелить обратно',
+        nextText: 15 //rasstilka posteli
+      }
+    ]
+  },
+  {
+    id: 11,
+    text: 'Молодец. Вы застелили дядину постель. А на полу нашли ржавый ключ.',
+    setState: {
+      rustyKey: true,
+      makeBed: 1
+    },
+    options: [{
+        text: 'Отойти от постели',
+        nextText: 12 //zal tut nado
       },
-      options: [{
-          text: 'Отойти от постели',
-          nextText: 12 //zal tut nado
-        },
-        {
-          text: 'Расстелить обратно',
-          nextText: 15 //rasstilka posteli
-        }
-      ]
-    }
+      {
+        text: 'Расстелить обратно',
+        nextText: 15 //rasstilka posteli
+      }
+    ]
   },
   {
     id: 12,
@@ -238,7 +246,7 @@ const textNodes = [{
     text: 'Вы тихо зашли в зал. Окна завешаны. Постель заправлена. Дяди нет.',
     options: [{
       text: 'Пойти на кухню',
-      nextText:
+      nextText: -1 //IZMENI
     }, {
       text: 'Расстелить постель',
       nextText: 15
@@ -297,5 +305,3 @@ const textNodes = [{
     ]
   }
 ]
-
-startGame()
